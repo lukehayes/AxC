bb05a5d1b7851aef05dcbc2d2813a0392f3015f9
lukehayes <lukepurplehayes@gmail.com>
2021-05-04 19:06:27 +0100

    Moved working implementation of FileIO.h into FileIO.c.

--- include/FileIO.h
+++ include/FileIO.h
@@ -6,7 +6,6 @@
 
 #include "Types.h"
 
-
 /**
  * Get the size of the file from a supplied file handle.
  *
@@ -14,14 +13,7 @@
  *
  * @return int
  */
-static int GetFileSizeFH(FILE* stream)
-{
-    fseek(stream, 0, SEEK_END);
-    int size = ftell(stream);
-    fseek(stream, 0, SEEK_SET);
-
-    return size;
-}
+static int GetFileSizeFH(FILE* stream);
 
 /**
  * Get the size of the file.
@@ -30,16 +22,7 @@ static int GetFileSizeFH(FILE* stream)
  *
  * @return int
  */
-static int GetFileSize(const char* filename)
-{
-    FILE* fh = fopen(filename, "r");
-    fseek(fh, 0, SEEK_END);
-    int size = ftell(fh);
-    fseek(fh, 0, SEEK_SET);
-    fclose(fh);
-
-    return size;
-}
+static int GetFileSize(const char* filename);
 
 /**
  * Read the contents of a file into a string. Allocates
@@ -50,31 +33,6 @@ static int GetFileSize(const char* filename)
  *
  * @return char*
  */
-void ReadFile(const char* filename, char** buffer)
-{
-    FILE* fh = fopen(filename, "r");
-    const int bufferSize = GetFileSizeFH(fh) + 1;
-    char data[bufferSize];
-
-    *buffer = malloc(sizeof(char) * bufferSize);
-
-    if(buffer == NULL)
-    {
-        printf("%s: Memory failed to allocate on line %d \n", __FILE__, __LINE__);
-    }
-
-    char c;
-    int i = 0;
-    while( (c = fgetc(fh)) != EOF )
-    {
-        /* TODO: Implement buffer read.  <02-05-21, Me> */
-        data[i] = c;
-        i++;
-    }
-
-    memmove(*buffer, data, bufferSize);
-
-    fclose(fh);
-}
+void ReadFile(const char* filename, char** buffer);
 
 #endif
---
+++ src/FileIO.c
@@ -0,0 +1,76 @@
+/**
+ * Abstraction for reading in file contents.
+ */
+#include "Types.h"
+
+/**
+ * Get the size of the file from a supplied file handle.
+ *
+ * @param FILE* file stream.
+ *
+ * @return int
+ */
+static int GetFileSizeFH(FILE* stream)
+{
+    fseek(stream, 0, SEEK_END);
+    int size = ftell(stream);
+    fseek(stream, 0, SEEK_SET);
+
+    return size;
+}
+
+/**
+ * Get the size of the file.
+ *
+ * @param FILE* file stream.
+ *
+ * @return int
+ */
+static int GetFileSize(const char* filename)
+{
+    FILE* fh = fopen(filename, "r");
+    fseek(fh, 0, SEEK_END);
+    int size = ftell(fh);
+    fseek(fh, 0, SEEK_SET);
+    fclose(fh);
+
+    return size;
+}
+
+/**
+ * Read the contents of a file into a string. Allocates
+ * memory, so must be freed.
+ *
+ * @param const char* filename
+ * @param char** buffer
+ *
+ * @return char*
+ */
+void ReadFile(const char* filename, char** buffer)
+{
+    FILE* fh = fopen(filename, "r");
+    const int bufferSize = GetFileSizeFH(fh) + 1;
+    char data[bufferSize];
+
+    *buffer = malloc(sizeof(char) * bufferSize);
+
+    if(buffer == NULL)
+    {
+        printf("%s: Memory failed to allocate on line %d \n", __FILE__, __LINE__);
+    }
+
+    extern int n;
+    printf("n: %i \n", n);
+
+    char c;
+    int i = 0;
+    while( (c = fgetc(fh)) != EOF )
+    {
+        data[i] = c;
+        i++;
+    }
+
+    memmove(*buffer, data, bufferSize);
+
+    fclose(fh);
+}
