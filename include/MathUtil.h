20859393375782d28f1dd2e33df8e07ca6f95623
lukehayes <lukepurplehayes@gmail.com>
2021-05-08 19:36:06 +0100

    Renamed Math.h to MathUtil.h

--- include/MathUtil.h
+++ include/MathUtil.h
@@ -8,15 +8,17 @@
 #ifndef AX_MATH_H
 #define AX_MATH_H
 
+#include <cglm/cglm.h>
 
 /**
  * Create and return a projection matrix.
  */
-void PerspectiveProjection()
+mat4* PerspectiveProjection(mat4* matrix)
 {
-    // TODO Implement perspective projection.
-}
+    glm_perspective(45.0f, 800.0f / 600.0f, 0.1f, 100.0f, *matrix);
+    return matrix;
 
+}
 /**
  * Create and return a orthographic matrix.
  */
