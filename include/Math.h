86485cf232b896530b3a283ba7f95bc5cbd56dd6
lukehayes <lukepurplehayes@gmail.com>
2021-05-08 19:08:34 +0100

    Added Math.h file to project.

---
+++ include/Math.h
@@ -0,0 +1,29 @@
+/**
+ * Ax Math Helpers.
+ *
+ * This code is loosely based around the adapter pattern
+ * and aims to be written in such a way that its inner
+ * library can be swapped out for another one easily.
+ */
+#ifndef AX_MATH_H
+#define AX_MATH_H
+
+
+/**
+ * Create and return a projection matrix.
+ */
+void PerspectiveProjection()
+{
+    // TODO Implement perspective projection.
+}
+
+/**
+ * Create and return a orthographic matrix.
+ */
+void OrthographicProjection()
+{
+    // TODO Implement orthographic projection.
+}
+
+
+#endif
