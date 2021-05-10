05f4e6cd90b655d0f3f19ca41bfe369b8d1a103d
lukehayes <lukepurplehayes@gmail.com>
2021-05-10 17:07:04 +0100

    VertexBuffer functionality is now accesible though function pointers.

--- include/VertexBuffer.h
+++ include/VertexBuffer.h
@@ -4,18 +4,78 @@
 #ifndef AX_VTXBUFFER_H
 #define AX_VTXBUFFER_H
 
-static GLuint vertexBufferID;
+typedef struct Buffer {
+    GLuint id;
+    void (*bind) ();
+    void (*unbind) ();
+    void (*destroy) ();
+
+} Buffer;
+
+
+/**
+ * Bind the currently bound buffer object.
+ *
+ * @param Buffer* buffer.
+ *
+ * @return void
+ */
+void BindBuffer(Buffer* buffer)
+{
+    glBindBuffer(GL_ARRAY_BUFFER, buffer->id);
+}
+
+/**
+ * Unbind the currently bound buffer object.
+ */
+void UnBindBuffer()
+{
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
+}
+
+/**
+ * Delete a vertex buffer from GPU memory.
+ *
+ * @return Buffer
+ */
+void DestroyVertexBuffer(Buffer* buffer)
+{
+    glDeleteBuffers(1, &buffer->id);
+}
+
+/**
+ * Create a Buffer struct.
+ *
+ * @return Buffer
+ */
+Buffer CreateBufferObject() 
+{
+    Buffer buffer;
+    buffer.bind = &BindBuffer;
+    buffer.unbind = &UnBindBuffer;
+    buffer.destroy = &DestroyVertexBuffer;
+    return buffer;
+}
 
 /**
  * Create and bind a vertex buffer object.
  */
-void CreateVertexBuffer()
+void CreateVertexBuffer(Buffer* buffer)
 {
-    glGenBuffers(1, &vertexBufferID);
-    glBindBuffer(GL_ARRAY_BUFFER, vertexBufferID);
+    glGenBuffers(1, &buffer->id);
+    glBindBuffer(GL_ARRAY_BUFFER, buffer->id);
 }
 
-void SetBufferData(int attributePosition, int vertexCount, const float verticies[12])
+/**
+ * Added the vertex data to the bound buffer object.
+ *
+ * @param int attributePosition
+ * @param int vertexCount
+ * @param float verticies[]
+ *
+ * @return Buffer*
+ */
+void SetBufferData(s32 attributePosition, s32 vertexCount, const float verticies[12])
 {
     glBufferData(GL_ARRAY_BUFFER, sizeof(float) * vertexCount, verticies, GL_STATIC_DRAW );
     glVertexAttribPointer(attributePosition, 3, GL_FLOAT, GL_FALSE, 0, (void*)0);
