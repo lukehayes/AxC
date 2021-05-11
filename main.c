ba33b2373795c1ca4b5a2d420e3203bd2e8a12e4
lukehayes <lukepurplehayes@gmail.com>
2021-05-11 14:20:32 +0100

    Removed old functions from main to clean it up a little.

--- main.c
+++ main.c
@@ -8,9 +8,6 @@
 #include "VertexBuffer.h"
 #include "Window.h"
 
-
-GLuint vao;
-GLuint buffer;
 float verticies[] = {
     -0.5, 0.5, 0.0f,
     -0.5, -0.5, 0.0f,
@@ -18,29 +15,15 @@ float verticies[] = {
     0.5, -0.5, 0.0f
 };
 
-void GL()
-{
-    /*CreateVertexArray();*/
-
-    /*Buffer* buffer = CreateBufferObject();*/
-    /*buffer->bind(buffer);*/
-
-    /*CreateVertexBuffer(buffer);*/
-
-    /*SetBufferData(0,12,verticies);*/
-
-    /*free(buffer);*/
-}
-
 int main(int argc, char *argv[])
 {
     Window window = CreateWindow(800,600, "Ax Window");
 
     Shader* shader = CreateShader();
 
-    CreateVertexArray();
+    VertexArray vao = CreateVertexArray();
 
-    Buffer buffer = CreateBufferObject(0,12,verticies);
+    VertexBuffer buffer = CreateVertexBuffer(0,12,verticies);
 
     float c = 0.0;
 
@@ -49,6 +32,7 @@ int main(int argc, char *argv[])
         c+= 0.01;
         /* Render here */
         glClear(GL_COLOR_BUFFER_BIT);
+
 
         /* Poll for and process events */
         glfwPollEvents();
@@ -62,8 +46,8 @@ int main(int argc, char *argv[])
         glfwSwapBuffers(window.handle);
     }
 
-    glfwTerminate();
     glfwDestroyWindow(window.handle);
+    glfwTerminate();
 
     return 0;
 }
