41e6255c5c555f8d5222f7dd9be8109fbddfac86
lukehayes <lukepurplehayes@gmail.com>
2021-05-12 15:03:27 +0100

    Added 3D projection and test models to main and vertex shader.

--- assets/shaders/default-vsh.glsl
+++ assets/shaders/default-vsh.glsl
@@ -2,9 +2,12 @@
 layout (location = 0) in vec3 aPos;
   
 out vec4 vertexColor;
+uniform mat4 projection;
+uniform mat4 model;
+uniform mat4 view;
 
 void main()
 {
-    gl_Position = vec4(aPos, 1.0);
-    vertexColor = vec4(0.5, 0.0, 0.0, 1.0);
+    gl_Position = projection * view * model * vec4(aPos, 1.0);
+    vertexColor = vec4(0.6, 0.6, 0.65, 1.0);
 }
--- main.c
+++ main.c
@@ -48,21 +48,69 @@ int main(int argc, char *argv[])
     f32 previousTime = glfwGetTime();
     f32 FPS = 60.0;
 
+    vec3 position = {0.0,0,0};
+
+    mat4 projection;
+    glm_mat4_identity(projection);
+    glm_perspective( glm_rad(90.0f), 800.0f/600.0f, 0.1f, 100.0f, projection );
+
+    mat4 view;
+    glm_mat4_identity(view);
+    vec3 eye = {0,0,3.0f};
+    vec3 center = {0,0,0};
+    vec3 up = {0,1.0f,0};
+    glm_lookat(eye, center,up,view);
+
+
+
+    mat4 model;
+    glm_mat4_identity(model);
+
+    glm_translate(model, position);
+
+    vec3 positions[10] = {
+        {0,0,-10.0f},
+        {3,0,-10.0f},
+        {3,3,-10.0f},
+        {1,3,-10.0f},
+        {10,3,-10.0f},
+        {-10,3,-10.0f},
+        {-3,23,-20.0f},
+        {-29,-13,-20.0f},
+        {12,-16,-20.0f},
+        {-20,33,-4.0f}
+    };
+
     while (!glfwWindowShouldClose(window.handle))
     {
-        c+= 0.01;
+        c+= 0.001;
         /* Render here */
         glClear(GL_COLOR_BUFFER_BIT);
+        glClearColor(0.8f, 0.8f, 0.8f, 1.0f);
 
         now = glfwGetTime();
-        gamestate.deltaTime = now - previousTime;
         previousTime = now;
-
 
         /* Poll for and process events */
         glfwPollEvents();
 
+        glm_rotate(model, glm_rad(sin(c)), (vec3){0,0,1});
+
         ShaderUniform2f(shader, "test", sin(c), cos(c));
+
+        ShaderUniformMat4(shader, "projection", projection);
+        ShaderUniformMat4(shader, "view", view);
+        ShaderUniformMat4(shader, "model", model);
+
+        for (int i = 0; i < 10; ++i) {
+            mat4 model;
+            glm_mat4_identity(model);
+
+            glm_translate(model, positions[i]);
+
+            ShaderUniformMat4(shader, "model", model);
+            glDrawArrays(GL_TRIANGLE_STRIP, 0, 4);
+        }
 
         UseShader(shader);
         glDrawArrays(GL_TRIANGLE_STRIP, 0, 4);
