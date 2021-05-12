5953cc425157241c86b70a6090fd47542da5b483
lukehayes <lukepurplehayes@gmail.com>
2021-05-12 14:07:28 +0100

    Added GameState.h to project.

---
+++ include/GameState.h
@@ -0,0 +1,16 @@
+/**
+ * A struct that holds global information about
+ * the game at a particular moment.
+ */
+#ifndef AX_GAMESTATE_H
+#define AX_GAMESTATE_H
+
+typedef struct GameState {
+
+    f32 deltaTime;
+    
+} GameState;
+
+GameState gamestate;
+
+#endif
