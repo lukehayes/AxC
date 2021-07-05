9fdde0ea7972040b96bf54ddc72933c362679323
lukehayes <lukepurplehayes@gmail.com>
2021-07-05 09:48:15 +0100

    Added include/Graphics.h to project.

---
+++ include/Graphics.h
@@ -0,0 +1,19 @@
+/**
+ * OpenGL/Graphics/Rendering abstractions.
+ */
+#ifndef AX_GFX_H
+#define AX_GFX_H
+
+/**
+ * Draw a square at a particular position.
+ */
+void Draw(float x, float y)
+{
+    // TODO
+    // Set x and y position inside the model matrix.
+    // Update the shader with this new model matrix.
+    // Call glDraw()... function to display the shape.
+}
+
+
+#endif
