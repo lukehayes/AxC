da6a48d3ee51a0638e9a31dffc2a048bc25c78b9
Luke Hayes <lukepurplehayes@gmail.com>
2021-12-24 09:35:26 +0000

    Added start of Camera3D implementation.

---
+++ include/Camera3D.h
@@ -0,0 +1,29 @@
+#ifndef CAMERA3D_H
+#define CAMERA3D_H
+
+typedef struct Camera3D
+{
+    vec3 position;
+
+} Camera3D;
+ 
+
+Camera3D* CreateCamera()
+{
+    Camera3D* camera = malloc(sizeof(Camera3D));
+
+    return camera;
+}
+
+void DestroyCamera3D(Camera3D* camera)
+{
+    if(camera != NULL)
+    {
+        free(camera);
+    }else
+    {
+        printf("Camera was NULL! Cannot be freed again. \n");
+    }
+}
+
+#endif
