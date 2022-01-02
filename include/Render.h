3a9b6307b4beec25858a45e22e0fa2af2db5f4dd
Luke Hayes <lukepurplehayes@gmail.com>
2022-01-02 16:28:40 +0000

    Added Clear() rendering function.

--- include/Render.h
+++ include/Render.h
@@ -1,11 +1,19 @@
 #ifndef AX_RENDER_H
 #define AX_RENDER_H
 
+void Clear()
+{
+    glClear(GL_COLOR_BUFFER_BIT);
+    glClearColor(0.3f, 0.3f, 0.3f, 1.0f);
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+}
+
 
 void GLDrawBasic(Shader* shader, int indices)
 {
     UseShader(shader);
-    glDrawArrays(GL_TRIANGLES, 0, indices);
+    glDrawArrays(GL_LINES, 0, indices);
 }
 
 
--- main.c
+++ main.c
@@ -76,9 +76,7 @@ int main(int argc, char *argv[])
 
         c+= 0.01;
         /* Render here */
-        glClear(GL_COLOR_BUFFER_BIT);
-        glClearColor(0.8f, 0.8f, 0.8f, 1.0f);
-        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+        Clear();
 
         now = glfwGetTime();
         previousTime = now;
