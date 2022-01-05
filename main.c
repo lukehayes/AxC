d12081cd1581f7f45fa27e53b6bcc0a74b7d0a66
Luke Hayes <lukepurplehayes@gmail.com>
2022-01-05 10:07:02 +0000

    Passthru shaders now using seperate projection and view matricies rather than a combined matrix.

--- main.c
+++ main.c
@@ -85,9 +85,8 @@ int main(int argc, char *argv[])
 
         glm_rotate(model->model, glm_rad(sin(c)), (vec3){1,1,1});
 
-        PrintMat4(camera->combined);
-
-        ShaderUniformMat4(shader, "combined", camera->combined);
+        ShaderUniformMat4(shader, "projection", camera->projection);
+        ShaderUniformMat4(shader, "view", camera->view);
         ShaderUniformMat4(shader, "model", model->model);
 
         RenderDrawBasic(vao->shader, 6);
