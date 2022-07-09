b25bd627ffca706bd30b70e10f4cc7b9f0b46d7f
Luke Hayes <lukepurplehayes@gmail.com>
2022-07-09 19:02:06 +0100

    render_init() now creating an OpenGL context.

--- include/Global.h
+++ include/Global.h
@@ -1,13 +1,17 @@
 #pragma once
 
+#include "Types.h"
+
 /**
  * Global state and variables for the game.
  */
 typedef struct global
 {
-   int       width;
-   int       height;
-   const_str title;
+   int         width;
+   int         height;
+   const_str   title;
+
+   GLFWwindow* window;
 
 } Global;
 
--- src/Render.c
+++ src/Render.c
@@ -6,6 +6,28 @@ void render_init(void)
 	global.width  = 640;
 	global.height = 480;
 	global.title  = "Game";
+
+	// Initialize GLFW
+	if(!glfwInit())
+	{
+		printf("GLFW Failed To Initialize \n");
+	}
+
+	// Set OpenGL
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+	global.window = glfwCreateWindow(
+						global.width,
+						global.height,
+						global.title,
+						NULL,
+						NULL);
+
+	glfwMakeContextCurrent(global.window);
+	gladLoadGLLoader((GLADloadproc) glfwGetProcAddress);
+
 }
 
 
