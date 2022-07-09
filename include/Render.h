4e718b36d0a877a163ac0260f4ecdcce29f37ca5
Luke Hayes <lukepurplehayes@gmail.com>
2022-07-09 17:56:23 +0100

    Removed RenderBasic function.

--- include/Render.h
+++ include/Render.h
@@ -4,8 +4,6 @@
 
 void RenderClear(float r, float g, float b);
 
-void RenderDrawBasic(Shader* shader, int indices);
-
 void RenderBegin();
 void RenderQuad();
 void RenderEnd();
--- src/Render.c
+++ src/Render.c
@@ -8,36 +8,6 @@ void RenderClear(float r, float g, float b)
 }
 
 
-void RenderDrawBasic(Shader* shader, int indices)
-{
-    UseShader(shader);
-    glDrawArrays(GL_TRIANGLES, 0, indices);
-}
-
 void RenderBegin() {}
-void RenderQuad() {
-    GLuint vao;
-    GLuint vbo;
-
-    float verticies[] = {
-        -1.0f, -1.0f, 0.0f,
-        1.0f, -1.0f, 0.0f,
-        0.0f,  1.0f, 0.0f,
-
-        -1.0f, 0.5f, 0.0f,
-        1.0f,  0.5f, 0.0f,
-        0.0f,  1.0f, 0.0f
-    };
-
-    glGenVertexArrays(1, &vao);
-    glBindVertexArray(GL_ARRAY_BUFFER, vao);
-
-    glGenBuffers(1, &vbo);
-    glBindBuffer(GL_ARRAY_BUFFER, vbo);
-
-    glBufferData(GL_ARRAY_BUFFER, sizeof(float) * 18, verticies, GL_STATIC_DRAW );
-    glVertexAttribPointer(0, 6, GL_FLOAT, GL_FALSE, 0, (void*)0);
-    glEnableVertexAttribArray(0);
-}
+void RenderQuad() {}
 void RenderEnd() {}
-
