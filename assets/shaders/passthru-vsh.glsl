1f8a7a5eaf036a483359be55f3cd1aa2dacbd106
Luke Hayes <lukepurplehayes@gmail.com>
2022-07-11 12:46:35 +0100

    Rewrote passthru shaders to be at their most basic.

--- assets/shaders/passthru-vsh.glsl
+++ assets/shaders/passthru-vsh.glsl
@@ -2,12 +2,9 @@
 layout (location = 0) in vec3 aPos;
   
 out vec4 vertexColor;
-uniform mat4 model;
-uniform mat4 projection;
-uniform mat4 view;
 
 void main()
 {
-    gl_Position = projection * view * model * vec4(aPos, 1.0);
-    vertexColor = vec4(0.6, 0.6, 0.65, 1.0);
+    gl_Position = vec4(aPos, 1.0);
+    vertexColor = vec4(0.5, 0.5, 0.5, 1.0);
 }
