c46239d8e2ec2b83ebd8ad624889ff7a0f147b37
Luke Hayes <lukepurplehayes@gmail.com>
2022-07-13 14:19:08 +0100

    Converted color values from integers to floats.

--- include/utils/colors.h
+++ include/utils/colors.h
@@ -2,35 +2,35 @@
 
 typedef struct Color
 {
-	int r;
-	int g;
-	int b;
+   float r;
+   float g;
+   float b;
 } Color;
 
-#define LIGHTGRAY  (Color){ 200, 200, 200 }   // Light Gray
-#define GRAY       (Color){ 130, 130, 130 }   // Gray
-#define DARKGRAY   (Color){ 80, 80, 80 }      // Dark Gray
-#define YELLOW     (Color){ 253, 249, 0 }     // Yellow
-#define GOLD       (Color){ 255, 203, 0 }     // Gold
-#define ORANGE     (Color){ 255, 161, 0 }     // Orange
-#define PINK       (Color){ 255, 109, 194 }   // Pink
-#define RED        (Color){ 230, 41, 55 }     // Red
-#define MAROON     (Color){ 190, 33, 55 }     // Maroon
-#define GREEN      (Color){ 0, 228, 48 }      // Green
-#define LIME       (Color){ 0, 158, 47 }      // Lime
-#define DARKGREEN  (Color){ 0, 117, 44 }      // Dark Green
-#define SKYBLUE    (Color){ 102, 191, 255 }   // Sky Blue
-#define BLUE       (Color){ 0, 121, 241 }     // Blue
-#define DARKBLUE   (Color){ 0, 82, 172 }      // Dark Blue
-#define PURPLE     (Color){ 200, 122, 255 }   // Purple
-#define VIOLET     (Color){ 135, 60, 190 }    // Violet
-#define DARKPURPLE (Color){ 112, 31, 126 }    // Dark Purple
-#define BEIGE      (Color){ 211, 176, 131 }   // Beige
-#define BROWN      (Color){ 127, 106, 79 }    // Brown
-#define DARKBROWN  (Color){ 76, 63, 47 }      // Dark Brown
+#define LIGHTGRAY     (Color){ 0.784, 0.784, 0.784 }
+#define GRAY          (Color){ 0.51, 0.51, 0.51 }
+#define DARKGRAY      (Color){ 0.314, 0.314, 0.314 }
+#define YELLOW        (Color){ 0.992, 0.976, 0.0 }
+#define GOLD          (Color){ 1.0, 0.796, 0.0 }
+#define ORANGE        (Color){ 1.0, 0.631, 0.0 }
+#define PINK          (Color){ 1.0, 0.427, 0.761 }
+#define RED           (Color){ 0.902, 0.161, 0.216 }
+#define MAROON        (Color){ 0.745, 0.129, 0.216 }
+#define GREEN         (Color){ 0.0, 0.894, 0.188 }
+#define LIME          (Color){ 0.0, 0.62, 0.184 }
+#define DARKGREEN     (Color){ 0.0, 0.459, 0.173 }
+#define SKYBLUE       (Color){ 0.4, 0.749, 1.0 }
+#define BLUE          (Color){ 0.0, 0.475, 0.945 }
+#define DARKBLUE      (Color){ 0.0, 0.322, 0.675 }
+#define PURPLE        (Color){ 0.784, 0.478, 1.0 }
+#define VIOLET        (Color){ 0.529, 0.235, 0.745 }
+#define DARKPURPLE    (Color){ 0.439, 0.122, 0.494 }
+#define BEIGE         (Color){ 0.827, 0.69, 0.514 }
+#define BROWN         (Color){ 0.498, 0.416, 0.31 }
+#define DARKBROWN     (Color){ 0.298, 0.247, 0.184 }
 
-#define WHITE      (Color){ 255, 255, 255 }   // White
-#define BLACK      (Color){ 0, 0, 0 }         // Black
-#define BLANK      (Color){ 0, 0, 0 }           // Blank (Transparent)
-#define MAGENTA    (Color){ 255, 0, 255 }     // Magenta
-#define RAYWHITE   (Color){ 245, 245, 245 }   // My own White (raylib logo)
+#define WHITE         (Color){ 1.0, 1.0, 1.0 }
+#define BLACK         (Color){ 0.0, 0.0, 0.0 }
+#define BLANK         (Color){ 0.0, 0.0, 0.0 }
+#define MAGENTA       (Color){ 1.0, 0.0, 1.0 }
+#define RAYWHITE      (Color){ 0.961, 0.961, 0.961 }
